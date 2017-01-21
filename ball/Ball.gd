2b2f841df9b7b4c70a5ab847f011ff8e57c4e0d2
Pascal Le Merrer <pascal.lemerrer@orange.com>
2017-01-21 15:09:40 +0100

    Clean ball script

--- ball/Ball.gd
+++ ball/Ball.gd
@@ -1,22 +1,11 @@
 extends RigidBody2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
 func _ready():
 	set_contact_monitor(true)
 	set_max_contacts_reported(5)
-	set_process(true)
-	print("started")
-
-
-func _process(delta):
-	if( get_colliding_bodies().size() > 0):
-		print ("Collision with ", get_colliding_bodies() ) 
 
 func _on_RigidBody2D_body_enter( body ):
-	print("collide with", body)
+
 	if(body extends StaticBody2D and body.is_goal()):
 		print("goal was hit")
 		queue_free()
